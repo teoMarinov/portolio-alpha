ec97c30f1d3b2209304a0e55d923765a177d38ae
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-02 15:41:49 +0200

    create content container

--- src/components/LandingPage/Container.tsx
+++ src/components/LandingPage/Container.tsx
@@ -1,7 +1,16 @@
+import Lottie from "lottie-react";
+import scroller from "./Scroll.json";
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className=" w-full h-full overflow-auto">{children}</div>
+      <div className="h-full bg-black flex justify-center relative ">
+        <div className=" absolute top-16 w-full max-h-[48rem] p-6 bg-neutral-100">
+          {children}
+        </div>
+        <div className=" size-10 absolute bottom-12">
+          <Lottie animationData={scroller}/>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -3,13 +3,16 @@ import Sidebar from "../SideBar/Sidebar";
 
 const LandingPage = () => {
   return (
-
     <div className="fixed flex h-screen w-screen overflow-x-hidden">
       <Sidebar />
-      <Container>
-        <div className="h-screen">a</div>
-        <div className="h-screen">a</div>
-      </Container>
+      <div className=" w-full h-full bg-gray-100 overflow-auto">
+        <Container>
+          <div className="h-screen">a</div>
+        </Container>
+        <Container>
+          <div className="h-screen">acC</div>
+        </Container>
+      </div>
     </div>
   );
 };
